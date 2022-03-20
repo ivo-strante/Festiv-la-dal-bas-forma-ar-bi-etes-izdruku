192bfcf47bd1b44258ee27a53fccf530575ca497
ivo-strante <ivo.strante@gmail.com>
2022-03-20 21:30:27 +0200

    izmaiņas

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,26 @@
 function printDiv() {
-    var printContents = document.getElementById("activity1").value;
+    const activityOption1Description="Aktivitātes opcija Nr1";
+    const activityOption2Description="Aktivitātes opcija Nr2";
+    const activityOption3Description="Aktivitātes opcija Nr3";
+    var opt = {
+        margin:       1,
+        filename:     'bilete.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 2 },
+        jsPDF:        { unit: 'mm', format: 'A4', orientation: 'portrait' }
+      };
+    if (document.getElementById("activity1").value=="activityOption1") {
+        var printContents = activityOption1Description;
+    }
+    
     var originalContents = document.body.innerHTML;
 
-    document.body.innerHTML = printContents;
-
-    window.print();
+ 
+    var element = document.getElementById("ticket");
+    html2pdf().set(opt).from(element).toPdf().get('pdf').then(function (pdf) {
+        window.open(pdf.output('bloburl'), '_blank');
+      });
+    //window.print();
 
     document.body.innerHTML = originalContents;
 }
